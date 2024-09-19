f0dada2ceb7204d26146d85bf646858f61b70ade
Simon Oulevay <simon.oulevay@gmail.com>
2024-09-19 22:53:37 +0200

    1.2.0

--- lib/gitenv.rb
+++ lib/gitenv.rb
@@ -1,7 +1,7 @@
 require 'paint'
 
 module Gitenv
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
 end
 
 Dir[File.join File.dirname(__FILE__), File.basename(__FILE__, '.*'), '*.rb'].each { |lib| require lib }
